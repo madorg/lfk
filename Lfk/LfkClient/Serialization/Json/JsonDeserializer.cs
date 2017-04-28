4093c6f9790bff13dfacd85bddebe554d3e8dfce
map <alx.magdich@gmail.com>
2017-04-28 21:58:45 +0300

    upd: Serialization/Json - add new methods to (de)serizalize to(from) necessary file

--- Lfk/LfkClient/Serialization/Json/JsonDeserializer.cs
+++ Lfk/LfkClient/Serialization/Json/JsonDeserializer.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using LfkClient.FileSystemControl;
 
 namespace LfkClient.Serialization.Json
 {
     class JsonDeserializer
     {
-        public static object DeserializeObject<T>(string data)
+        public static T DeserializeObjectFromFile<T>(string fileName)
+        {
+            string oldData = FileSystem.ReadFileContent(fileName);
+            T deserializedOldData = DeserializeObject<T>(oldData);
+            return deserializedOldData;
+        }
+
+        public static T DeserializeObject<T>(string data)
         {
             return JsonConvert.DeserializeObject<T>(data);
         }
--- Lfk/LfkClient/Serialization/Json/JsonSerializer.cs
+++ Lfk/LfkClient/Serialization/Json/JsonSerializer.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using LfkClient.FileSystemControl;
 
 namespace LfkClient.Serialization.Json
 {
     class JsonSerializer
     {
+        public static void SerializeObjectToFile<T>(T data, string fileName)
+        {
+            string newData = SerializeObject(data);
+            FileSystem.WriteToFile(fileName, newData);
+        }
+
         public static string SerializeObject(object obj)
         {
             string result = string.Empty;
 
-            result = JsonConvert.SerializeObject(obj);
+            result = JsonConvert.SerializeObject(obj, Formatting.Indented);
 
             return result;
         }
